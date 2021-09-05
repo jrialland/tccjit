4caea1fe4a043b3da17b550d97eb9b23ed1260d0
jrialland <julien.rialland@gmail.com>
2021-09-05 18:35:40 +0200

    try to write libtcc1.a on disk if absent

--- src/tccjit.cpp
+++ src/tccjit.cpp
@@ -1,7 +1,46 @@
 #include "tccjit.hpp"
 
 #include <cstring>
+#include <cstdio>
+#include <algorithm>
 #include <stdexcept>
+#include <fstream>
+#include <filesystem>
+#include <iostream>
+using namespace std;
+namespace fs = std::filesystem;
+
+bool is_file(const std::string &name) {
+	std::ifstream f(name.c_str());
+	return f.good();
+}
+
+extern uint8_t libtcc1_a_data[];
+extern unsigned libtcc1_a_size;
+
+void write_file(uint8_t* data, unsigned len, const char * dest) {
+	FILE *f = fopen(dest, "wb");
+	if(f == nullptr) {
+		throw std::runtime_error(strerror(errno));
+	}
+	unsigned done = 0;
+	while(done < len) {
+		unsigned thisround = std::min(1024u, len-done);
+		int written = fwrite(data + done, sizeof(uint8_t), thisround, f);
+		if(written != thisround) {
+			throw std::runtime_error(strerror(errno));
+		}
+		done += written;
+	}
+	fclose(f);
+}
+
+void ensure_libtcc1a() {
+	std::string path = "./libtcc1.a";
+	if(!is_file(path)) {
+		write_file(libtcc1_a_data, libtcc1_a_size, path.c_str());
+	}
+}
 
 #ifdef __linux 
 #include <libgen.h>
@@ -18,12 +57,17 @@ extern "C" {
 
 jit::Module::Module(const std::string &code) {
 	
+	ensure_libtcc1a();
+
 	tcc = std::shared_ptr<TCCState>(tcc_new(), tcc_delete);
 	
 	if(tcc.get() == nullptr) {
 		throw std::runtime_error("tcc context initialization failed");
 		return;
 	}
+
+	tcc_set_lib_path(tcc.get(), ".");
+
 	
 	tcc_set_output_type(tcc.get(), TCC_OUTPUT_MEMORY);
 
